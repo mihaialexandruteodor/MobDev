e0048aaaf0fa1c08a5f5d63e4d29f504285ea113
mihaiteodor97 <mihaialexandruteodor@gmail.com>
2021-06-01 14:10:08 +0300

    created data obj for firebae

---
+++ UniBucNavigator/app/src/main/java/com/mihaialexandruteodor406/unibucnavigator/ui/model/MarkerData.kt
@@ -0,0 +1,11 @@
+package com.mihaialexandruteodor406.unibucnavigator.ui.model
+
+class MarkerData(var wpName: String, var wpDescript: String, var wpLat: Double, var wpLon: Double) {
+
+    val waypointName: String = wpName
+    val waypointDescript: String = wpDescript
+    val lat: Double = wpLat
+    val lon: Double = wpLon
+
+
+}
