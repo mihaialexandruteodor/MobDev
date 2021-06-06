a6dc319903b1781fd0ff660e6ef3bd1a7f9f3ad2
mihaiteodor97 <mihaialexandruteodor@gmail.com>
2021-06-06 12:36:39 +0300

    related to last

--- UniBucNavigator/app/src/main/java/com/mihaialexandruteodor406/unibucnavigator/ui/PhotoActivity.kt
+++ UniBucNavigator/app/src/main/java/com/mihaialexandruteodor406/unibucnavigator/ui/PhotoActivity.kt
@@ -1,13 +1,82 @@
 package com.mihaialexandruteodor406.unibucnavigator.ui
 
 import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import com.mihaialexandruteodor406.unibucnavigator.R
+import com.mihaialexandruteodor406.unibucnavigator.R.layout.activity_poze
+import java.io.ByteArrayOutputStream
+
 
 class PhotoActivity : Activity() {
+
+    lateinit var imageView: ImageView
+    private val CAMERA_REQUEST = 1888
+    private val MY_CAMERA_PERMISSION_CODE = 100
+    lateinit var photoButton: Button
+    lateinit var shareButton: Button
+    lateinit var photo: Bitmap
+
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_poze)
+        setContentView(activity_poze)
+        imageView = findViewById(R.id.imageView)
+        photoButton = findViewById<View>(R.id.butonPoza) as Button
+        photoButton.setOnClickListener{
+            if (checkSelfPermission(android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(android.Manifest.permission.CAMERA), MY_CAMERA_PERMISSION_CODE)
+            } else {
+                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(cameraIntent, CAMERA_REQUEST)
+            }
+        }
 
+        shareButton = findViewById<View>(R.id.butonShare) as Button
+        shareButton.setOnClickListener{
+            val shareIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_STREAM,  getImageUri(applicationContext, photo))
+                type = "image/jpeg"
+            }
+            startActivity(Intent.createChooser(shareIntent,"Da share la aceasta imagine!"))
+        }
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == MY_CAMERA_PERMISSION_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show()
+                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(cameraIntent, CAMERA_REQUEST)
+            } else {
+                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            photo = (data.extras!!["data"] as Bitmap?)!!
+            imageView.setImageBitmap(photo)
+            shareButton.visibility = View.VISIBLE
+        }
+    }
+
+    fun getImageUri(inContext: Context, inImage: Bitmap): Uri? {
+        val bytes = ByteArrayOutputStream()
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
+        val path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null)
+        return Uri.parse(path)
+    }
+
 }
